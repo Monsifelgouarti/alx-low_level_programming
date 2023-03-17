d2801beec2f52c18509f646d241f8b7933c3293d
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-16 18:10:24 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main(void)? (Disply message)
+ *
+ * Description: prints a line with printf()
+ * Return: 0
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
