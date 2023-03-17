97d1fa9932dc1c198576aac683f1fdbfcac91b76
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-16 17:47:43 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
-int main()
+/**
+ * main(void)? (Disply message)?
+ *
+ * Description: prints a line with puts()
+ */
+int main(void)#function main
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	/**
+	 * return 0
+	 */
+	return (0);
 }
