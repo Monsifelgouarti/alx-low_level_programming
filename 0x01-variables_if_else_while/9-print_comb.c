70d5d9799a83b75c4499d848fe30192dc51ae85c
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-17 19:29:27 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main(void)? (-print combinations)
+ * Description: use function putchar() 
+ * prints all possible combinations of single-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
