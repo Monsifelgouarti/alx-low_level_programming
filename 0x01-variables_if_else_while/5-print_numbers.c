390e7bb6ec85a04e024b697802d1fb4f55c629b6
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-17 18:36:02 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main(void)? (-print digit)
+ * Description: prints all single digit numbers
+ * of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf(i)
+		i++;
+	}
+	printf('\n');
+	return (0);
+}
