50b3df608d42a257248bf5d97f3e7341386079bc
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-17 19:52:13 -0700

    print combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main(void)? (-puts alphabets)
+ * Description: use function puts() to print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == j)
+				continue;
+			else{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
