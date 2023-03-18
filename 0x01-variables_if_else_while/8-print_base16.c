adb516f082025cf83d9810cfa1f95d30be329610
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-17 19:21:31 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main(void)? (-prints Hexadecimal)
+ * Description: use function putchar() to print Hexadecimal
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) +'0');
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
