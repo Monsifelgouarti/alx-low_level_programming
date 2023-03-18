6b27ba75364a0c345caee4ea1739c024a351ef92
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-17 18:06:30 -0700

    the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,8 +6,11 @@
  */
 int main(void)
 {
-	for (int i = 'a', i <= 'z', i++)
-		putchar(i);
+	char alphabet[26];
+
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	for (int i = 0, i < 26, i++)
+		putchar(alphabet[i]);
 	putchar('\n')
 	return (0);
 }
