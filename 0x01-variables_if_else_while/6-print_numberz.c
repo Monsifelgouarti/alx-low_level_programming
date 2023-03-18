f69b6faf4e6e34a2fd5940399950bc31e34f48d5
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-17 18:47:30 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main(void)? (-print digit)
+ * Description: prints all single digit numbers
+ * of base 10 starting from 0 using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar((char) i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
