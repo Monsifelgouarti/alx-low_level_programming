86fb94a95084875bd408bccd92bc7f6a40db4d37
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-17 18:00:38 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main(void)? (-puts alphabets)
+ * Description: use function puts() to print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 'a', i <= 'z', i++)
+		putchar(i);
+	putchar('\n')
+	return (0);
+}
