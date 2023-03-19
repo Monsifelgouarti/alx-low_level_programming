aa9bd2d9e2a25bc8b7015c3d6ba691a2b9046033
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-19 05:19:34 -0700

    print_combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,13 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 		for (j = 0; j <= 9; j++)
 		{
-			if (i == j || i > j)
-				continue;
-			else
+			if (i != j && i < j)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
+				putchar(',');
+				putchar(' ');
 			}
-			putchar(',');
-			putchar(' ');
 		}
 	putchar('\n');
 	return (0);
