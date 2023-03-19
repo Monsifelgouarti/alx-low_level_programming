afc141545550c07afffb16f8c6dc45b100911601
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-19 04:42:16 -0700

    UNIX simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main(void)? (- Display a quote)
  * Description: Print a message to standar error
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fwrite(&quote, sizeof(quote), 1, stderr);
 	return (1);
