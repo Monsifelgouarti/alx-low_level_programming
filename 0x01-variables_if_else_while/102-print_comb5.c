0a9f5af2840c36cef2f09d246d37f74ccbd45bc9
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-19 09:47:21 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main(void)? (-puts alphabets)
+ * Description: prints all possible different combinations
+ * of two tow-digits numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 9; j++)
+			for (k = 0; k <= 9; k++)
+				for (l = 0; l <= 9; l++)
+					if (i != j && j != k && i < j && j < k)
+					{
+						putchar((i % 10) + '0');
+						putchar((j % 10) + '0');
+						putchar(' ');
+						putchar((k % 10) + '0');
+						putchar((l % 10) + '0');
+						if (k == 9 && j == 8 && i == 7)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+	putchar('\n');
+	return (0);
+}
