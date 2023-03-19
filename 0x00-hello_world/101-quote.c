4aacc1efd730c87a511763204487c68c265070d7
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-19 04:20:05 -0700

    UNIX simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
+	char quote [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fwrite(&quote, 59, 1, stderr);
 	return (1);
 }
