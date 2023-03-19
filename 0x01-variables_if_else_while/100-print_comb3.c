ca05f42ca3b1653f532169d4b284f07510565c49
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-19 05:22:07 -0700

    print_combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,8 +15,11 @@ int main(void)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
-				putchar(',');
-				putchar(' ');
+				if (j < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	putchar('\n');
