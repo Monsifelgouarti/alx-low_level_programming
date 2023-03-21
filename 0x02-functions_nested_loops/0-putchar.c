6a3190d26e9c7126726d776aadd8a501fca7015a
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-21 16:36:30 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main(void)? - print _putchar
+ * Description: prints _putchar, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	fwrite("_putchar\n", 9, 1, stdout);
+	return (0);
+}
