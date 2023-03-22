8a784f0e15a43ab932f6f0d6f804f1c01054bf6b
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-22 16:17:46 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,11 @@
 int main(void)
 {
 	char str[] = "_putchar";
-	int i = 0;
+	int i;
 
-	while (str[i] != '');
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
 	_putchar('\n');
 	return (0);
