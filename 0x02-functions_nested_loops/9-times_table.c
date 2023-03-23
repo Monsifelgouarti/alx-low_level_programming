70ac772a5f8957baa63808e2dc65f79598a040bc
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-23 16:00:38 -0700

    9 times tabele

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,21 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		for (j = 1; j < 10; j++)
 		{
 			res = i * j;
+			_putchar(',');
+			_putchar(' ');
 			if (res < 10)
 			{
-				_putchar((res % 10) + '0');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
 			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (res < 10 && j < 9)
-				_putchar(' ');
+			_putchar((res % 10) + '0');
 		}
 		_putchar('\n');
 	}
