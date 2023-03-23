1bcf3876d558dce2d4539b489dc0164f76ed41fc
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-23 08:37:49 -0700

    munites of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_baeur(void)? (- print minutes)
+ * Description: print every munite of the day
+ * Return: 0
+ */
+void jack_baeur(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+}
