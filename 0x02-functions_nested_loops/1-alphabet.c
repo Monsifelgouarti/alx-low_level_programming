dfb212a2cb292af0cca03ae1ea3aa3358840466f
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-22 17:30:41 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,13 @@
  *  followed by a new line.
  *  Return: 0
  */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	_putchart(i);
+	_putchar('\n');
+}
 int main(void)
 {
 	print_alphabet();
