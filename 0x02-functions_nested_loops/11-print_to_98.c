469042a85eccbd8d2b518924b4c1f7fbf8d0f461
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-23 16:21:12 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_to_98(int n)? (- print numbers)
+ * Description: print Numbers form a given n to 98
+ * @n: a number
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+		do
+		{
+			printf("%d,", n);
+			if (n != 98)
+			_putchar(' ');
+			n++;
+		}while(n <= 98);
+	else
+		do
+		{
+			printf("%d,", n);
+			if (n != 98)
+			_putchar(' ');
+			n--;
+		}while(n >= 98)
+}
