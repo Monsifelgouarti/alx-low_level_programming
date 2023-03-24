788f59bffa34611e34cdffc4d0aa3bdf1162c41b
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-24 04:05:25 -0700

    fibonacci 50

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 	int i;
 	long int fn_1 = 1, fn_2 = 0, f;
 
-	for (i = 2; i <= 50; i++)
+	for (i = 2; i <= 51; i++)
 	{
 		f = fn_2 + fn_1;
 		printf("%ld", f);
 		fn_2 = fn_1;
 		fn_1 = f;
-		if (i < 50)
+		if (i < 51)
 		printf(", ");
 	}
 	printf("\n");
