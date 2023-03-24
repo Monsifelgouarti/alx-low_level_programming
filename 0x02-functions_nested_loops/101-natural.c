63b42fc09f7403c64e676af7d02c3080b44adf29
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-24 03:37:32 -0700

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * void main(void);? (- print somme)
- * Description: print the sum of all the multiples 
+ * main(void);? (- print somme)
+ * Description: print the sum of all the multiples
  * of 3 or 5 below 1024
  * Return: 0
  */
