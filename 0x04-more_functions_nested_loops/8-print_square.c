13217232807880c2459ba63e1fe58ed28deeb1d3
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-24 18:15:27 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_quareint)? (- print a square)
+ * print_square(int)? (- print a square)
  * Description:  prints a square of '#'
  * @n: the size of the square.
  */
-void print_diagonal(int n)
+void print_square(int n)
 {
 	int i, j;
 
 	if (n > 0)
 	for (i = 1; i <= n; i++)
 	{
-		for (j = 1; j <=n; j++)
+		for (j = 1; j <=n ; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
