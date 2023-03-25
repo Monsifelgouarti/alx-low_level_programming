68094e568a4fa1f051535252cdc1d558673829b2
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-25 15:04:46 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,9 +11,12 @@ void print_triangle(int n)
 	if (n > 0)
 	for (i = 1; i <= n; i++)
 	{
-		for (j = 1; j <= n; j++)
+		for (j = n - i; j <= j; j--)
+			_putchar(' ');
+		for (j = 1; j <= i; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
