1c8e3cf64113ef60d68df000469144f08de01c79
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-26 08:07:55 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main(void)? (- print the prime factor)
+ * Description:  prints the largest prime factor
+ */
+void main(void)
+{
+	long int num = 612852475143;
+	int i, divador, largest_p_f;
+
+	divador = 1;
+	while ()
+	{
+		for (j = (n - i); j >= 1; j--)
+			_putchar(' ');
+		for (k = 1; k <= i; k++)
+			_putchar('#');
+		_putchar('\n');
+		i++;
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number(int)? (- print integer)
+ * Description:  print a number 
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	int res;
+
+	res = n / 10;
+	if (n >= 10)
+		print_number(res);
+	else
+		_putchar(n + '0');
+
+	_putchar((n % 10) + '0');
+}
