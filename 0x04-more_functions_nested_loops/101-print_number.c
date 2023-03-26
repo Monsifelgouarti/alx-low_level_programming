72a05a611675829cb46466abd802718c6c711fc1
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-26 08:16:59 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_number(int)? (- print integer)
- * Description:  print a number 
+ * Description:  print a number
  * @n: number to print
  */
 void print_number(int n)
@@ -10,7 +10,10 @@ void print_number(int n)
 
 	res = n / 10;
 	if (n >= 10)
+	{
 		print_number(res);
+		_putchar((n % 10) + '0');
+	}
 	else
 		_putchar(n + '0');
 }
