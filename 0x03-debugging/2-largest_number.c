d534c0ee9ce37424bd958e18e51fd5190a079f72
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-28 17:02:37 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,14 +14,14 @@ int largest_number(int a, int b, int c)
 
 	if (a > b)
 	{
-		if (b > c && a > c)
+		if (b > c || a > c)
 			largest = a;
 		else
 			largest = c;
 	}
 	else if (b > a)
 	{
-		if (b > c && a > c)
+		if (b > c || a > c)
 			largest = b;
 		else
 			largest = c;
