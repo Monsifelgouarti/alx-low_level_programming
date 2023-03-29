0f1310836522f4cbc08136a0226464aa00c196dd
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-29 16:56:53 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98 - update the value of a pointer 
+ * @*n: a pointer to an int
+ * Return: nothing.
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory contains functions manipulating arrays pointers and Strings.
