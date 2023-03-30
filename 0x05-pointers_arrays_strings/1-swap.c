4b617ff7dba7bfaf470d32b1039c8e44ab73823c
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-29 17:02:54 -0700

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - change value of to integers
+ * @a: a pointer to an integer
+ * @b: a pointer to an integer
+ * Return: nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	c = *a;
+	*a = *b;
+	*b = c:
+}
