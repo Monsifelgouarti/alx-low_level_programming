a5401307f0fa4bda94616bbfca0b329b4cfc7126
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-29 17:23:26 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen  - change value of to integers
+ * @s: a pointer to a String
+ * Return: tength of s.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
