ca381188f00ef1da8e591b3e8c2648e6c4486bf9
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-30 16:58:24 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * _strncat - concatenates a string to another
+ * @dest: String that wiil contais concatinated strings
+ * @src: String that will be appended to @dest
+ * @n: bytes from @src
+ * Return: a pointer to conatinated String.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0, j, src_len = 0;
+
+	while (*(dest + dest_len))
+	{
+		dest_len++;
+	}
+
+	for (j = 0; j <= n && *(src + j); j++)
+	{
+		*(dest + dest_len + j) = *(src + j);
+	}
+
+	return (dest);
+}
