c5c149436bf80df479f6cc97ac43ca24d267ee56
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-29 17:32:20 -0700

    puts String

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _puts  - print a string
+ * @s: a pointer to a String
+ * Return: nothing.
+ */
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		putchar(*(s + i));
+		i++;
+	}
+}
