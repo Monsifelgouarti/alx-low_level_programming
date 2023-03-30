68074905ad59b174de32526ab99c66a191b2a6e5
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-30 15:51:35 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * rev_string - appends a string to another
+ * _strcat - appends a string to another
  * @dest: String that wiil contais concatinated strings
  * @src: String that will be appended to @dest
  * Return: a pointer to conatinated String.
@@ -19,12 +19,12 @@ char *_strcat(char *dest, char *src)
 		src_len++;
 	}
 
-	for (j = dest_len - 1; j >= (dest_len + src_len -1); j++)
+	for (j = dest_len - 1; j <= (dest_len + src_len - 1); j++)
 	{
 		*(dest + j) = *(src + k);
 		k++;
 	}
-	*(dest + k) = '0';
+	*(dest + k + 1) = '0';
 
-	return (*dest);
+	return (dest);
 }
