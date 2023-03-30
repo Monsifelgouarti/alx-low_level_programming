98599f0040a6a0aec20764ccf68e34cc4de48ba0
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-30 06:40:20 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * rev_string - reverse a string
+ * @s: a pointer to a String
+ * Return: nothing.
+ */
+void rev_string(char *s)
+{
+	char *rev_s;
+	int i = 0, j, k = 0;
+
+	while (*(s + i))
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		*(rev_s +k) = *(s + j);
+		k++;
+	}
+	s = rev_s;
+}
