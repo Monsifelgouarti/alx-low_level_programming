5b1d36381d4da221577205bb685a82f6f80b4f1f
Monsifelgouarti <monsif.cun@gmail.com>
2023-03-31 16:12:41 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * *leet - changes "aAeEoOlLtT" with equivalent "4433001177"
+ * @str: String
+ * Return: string modified.
+ */
+char *leet(char *str)
+{
+	int i = 0, j;
+	char leet[] = "aAeEoOlLtT", num[] = "4433001177";
+
+	while (*(str + i) != '\0')
+	{
+		for (j = 0; j < 9; j++)
+			if (*(str + i) == leet[j])
+				*(str + i) = num[j];
+		i++;
+	}
+
+	return (str);
+}
