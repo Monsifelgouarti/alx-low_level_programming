00a03c0dfa4cf3bc238917b837d98f5f03d724cc
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-01 07:28:26 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * *print_number - print numbers with putchar
- * @str: Integer
+ * @n: Integer
  * Return: nothing.
  */
 int print_number(int n)
 {
 	int res;
-	
+
 	if (n >= 0)
 	{
 		if (n >= 10)
@@ -27,7 +27,7 @@ int print_number(int n)
 		{
 			res = n / 10;
 			print_number(res);
-			_putchar((n % 10) * -1 + '0');
+			_putchar(i((n % 10) * -1) + '0');
 		}
 
 	}
