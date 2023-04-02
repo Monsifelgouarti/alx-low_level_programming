7c3612125013c8d9ac37c2a1b7653b14a3613a14
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-02 08:42:33 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * puts_half  - print every other char of a string
+ * @s: a pointer to a String
+ * Return: nothing.
+ */
+void puts_half(char *s)
+{
+	int i = 0, j, n;
+
+	while (*(s + i))
+	{
+		i++;
+	}
+
+	if (i % 2 == 1)
+		i--;
+	n = i / 2;
+	for (j = n; j < i; j++)
+		putchar(*(s + j));
+	putchar('\n');
+}
