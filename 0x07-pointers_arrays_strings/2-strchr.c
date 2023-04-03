b89e3ca78efe330a213b871938caeff33ccbd4e8
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-03 16:55:37 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * _strchr - locate a charter in a string
+ * @s: array of char
+ * @c: char to locate
+ * * Return: a pointer tosccurence of c
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = NULL;
+	int i = 0;
+
+	while (s[i])
+	{
+		if (*(s + i) == c)
+		{
+			p = *(s + i);
+			break;
+		}
+		i++;
+	}
+
+	return (p);
+}
