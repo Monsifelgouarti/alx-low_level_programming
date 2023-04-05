e1414f35ad09c937b93344cfdfc228d36295bd88
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-04 17:01:31 -0700

    diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * print_diagsums - print sums of diag of a matrix
+ * @a: a matrix of integers
+ * size: size of matrix
+ * * Return: nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, diagsums = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			if ( i == j)
+				diagsums += a[i][j];
+		printf("%d, ",diagsums);
+		a++;
+	}
+
+	for (i = size - 1;i >= 0; i--)
+	{
+		for (j = size - 1; j >= 0; j--)
+			if ( i == j)
+				diagsums += a[i][j];
+		a--;
+	}
+		printf("%d\n",diagsums);
+	
+
+}
