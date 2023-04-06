75ee76db45faac6bbeed45088e7f7d60293fbe31
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-05 17:22:24 -0700

    print rev string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - print a string in reverse with recursion
+ * @s: a strign to print
+ * * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_rrint_rev_recursion(s);
+		putchar(*s);
+	}
+	else
+		putchar('\n');
+}
