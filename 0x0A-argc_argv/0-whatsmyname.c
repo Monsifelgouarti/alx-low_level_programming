e97864eea9c6fa41166e994281ab7d8dafe671ed
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-08 07:04:56 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,9 @@
  * main - it prints it's name
  * @argc: number of arguments passed to the program
  * @argv: array of arguments
- * return: 0.
+ * Return: 0.
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
