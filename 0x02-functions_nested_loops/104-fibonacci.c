0e06dbd996cf3316bd7690af1c3bd6f3cb5d9d1b
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-10 06:33:34 -0700

    fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,19 @@
 int main(void)
 {
 	int i;
-	long int fn_1 = 1, fn_2 = 0, f = 0, sum = 0;
+	long long int fn_1 = 1, fn_2 = 0, f = 0;
 
+	printf("%lld, ", fn_1);
+	printf("%lld, ", fn_2);
 	for (i = 2; i <= 98; i++)
 	{
 		f = fn_2 + fn_1;
-		if ((f % 2) == 0)
-			sum = sum + f;
+		printf("%lld, ", f);
 		fn_2 = fn_1;
 		fn_1 = f;
+		if(i < 98)
+		printf(", ");
 	}
-	printf("%ld", sum);
 	printf("\n");
 	return (0);
 }
