bc3bf8161f135ca15801fe4daea3b5afecd83fcd
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-11 17:20:24 -0700

    create array malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,15 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 
 	if (size <= 0)
-	       return (NULL);
+		return (NULL);
+
 	ptr = malloc(sizeof(char) * size);
-	
+
 	if (ptr == NULL)
 		return (NULL);
-	else
-	{
+
 		for (i = 0; i < size; i++)
 			ptr[i] = c;
-	}
+
 	return (ptr);
 }
