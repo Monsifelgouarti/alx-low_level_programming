6a1ef00f35e1015f82e5fb12d0f840a1670cd7c8
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-11 18:11:04 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 	int i , *ptr;
 
 	if (width == 0 || width < 0 || height == 0 || height < 0)
-		return (NULL)
+		return (NULL);
 
 	ptr = malloc(sizeof(char) * width);
 
