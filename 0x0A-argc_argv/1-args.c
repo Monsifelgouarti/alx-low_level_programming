01523c28e8233d8299025efe50c3021cbbba729f
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-14 10:42:48 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,10 @@
 /**
  * main - it prints the number of arguments 
  * @argc: number of arguments passed to the program
- * @argv: array of parameters
  * Return: 0.
  */
-int main(int argc, char *argv[])
+int main(int argc)
 {
-	char *ptr = argv;
-
 	printf("%d\n", argc - 1);
 	return (0);
 }
