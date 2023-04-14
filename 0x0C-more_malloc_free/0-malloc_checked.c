031c63b22847dd8ab1a3ed123205123b5e4355b7
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-14 16:51:41 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *malloc_checked - allocate memory using malloc
+ * @b: unsigned integer
+ * Return: a pointer of allocated memory or 98 if it fails.
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		return (98);
+	else
+		return (ptr)
+}
