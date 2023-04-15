e6c8c75177625379d8a8ba52abe1494db5d3a8fc
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-14 17:15:34 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,9 +9,9 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(sizeof(*ptr) * b);
+	*ptr = malloc(b);
 	if (ptr == NULL)
-		return (98);
+		exit (98);
 	else
 		return (ptr);
 }
