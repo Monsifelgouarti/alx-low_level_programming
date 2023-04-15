e2098625d50ce4e31ea8e48482c97524bf4dd340
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-15 08:40:13 -0700

    string n concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,12 +12,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 	unsigned int lens1 = 0, lens2 = 0, i = 0, size;
 
-	if (s1 != NULL)
-		while (s1[lens1])
-			lens1++;
-	if (s2 != NULL)
-		while (s2[lens2])
-			lens2++;
+	if (s1 == NULL)
+		s1 = "";
+	lens1 = strlen(s1);
+	if (s2 == NULL)
+		s2 = "";
+	lens2 = strlen(s2);
 	if (lens2 <= n)
 		size = lens1 + lens2;
 	else
@@ -27,13 +27,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	ptr = malloc(sizeof(char) * (size + 1));
 	if (ptr == NULL)
 		return (NULL);
-
-	if (lens1 != 0)
-	while (s1[i])
-	{
+	for (i = 0; i < lens1; i++)
 		ptr[i] = s1[i];
-		i++;
-	}
+	
 	for (i = 0; i < size; i++)
 		ptr[lens1 + i] = s2[i];
 	ptr[i] = '\0';
