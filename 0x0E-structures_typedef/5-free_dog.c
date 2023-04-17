3011a54e090e34db7add140d9d954f7366c6eaed
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-17 11:35:04 -0700

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - dealocate a dog
+ * @d: a pointer to a dog.
+ * Return: nothing.
+ */
+void free_dog(dog_t *d);
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
