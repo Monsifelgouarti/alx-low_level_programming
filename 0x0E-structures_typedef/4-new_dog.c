4e5372965a0c8dc1943c833068822b3cfe4619fe
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-18 17:44:11 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,8 +32,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(new_dog);
 			return (NULL);
 		}
-		new_dog->name = name;
-		new_dog->owner = owner;
+		strcpy(new_dog->name, name);
+		strcpy(new_dog->owner, owner);
 		new_dog->age = age;
 		return (new_dog);
 	}
