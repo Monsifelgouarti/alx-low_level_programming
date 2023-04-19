c5f6e4168277f2e45fa28264634589a34f7f06e2
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-18 17:29:50 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,11 +14,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
-		free(new_dog);
+		return (NULL);;
 	if (new_dog != NULL)
 	{
 		new_dog->name = malloc(strlen(name));
+		if (new_dog->name == NULL)
+		{
+			free(new_dog->name);
+			free(new_dog);
+			return (NULL);
+		}
 		new_dog->owner = malloc(strlen(owner));
+		if (new_dog->owner == NULL)
+		{
+			free(new_dog->owner);
+			free(new_dog);
+			return (NULL);
+		}
 		strcpy(new_dog->name, name);
 		strcpy(new_dog->owner, owner);
 		new_dog->age = age;
