7f6a92c7d16f649a31d046e3ba2c3603301d81d5
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-27 17:47:56 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+/**
+ * list_len -  returns the number of elements in a linked list
+ * @h: linked list
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	const list_t *ptr;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		len++;
+		ptr = ptr->next;
+	}
+	return (len);
+}
