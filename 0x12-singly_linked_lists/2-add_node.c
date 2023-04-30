fa4c65e5e426fd7edd08a5ca079b5845e1d6bdee
Monsifelgouarti <monsif.cun@gmail.com>
2023-04-30 09:20:50 -0700

    list len

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * add_node -  add a new node at the begining of a list_t list
+ * @head: the head of the list
+ * @str: string to initialize a new node
+ * Return: the address of the new element.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *s;
+	const list_t *ptr;
+
+	s = malloc(sizeof(char) * strlen(str));
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL || s == NULL)
+		return (NULL);
+	s = strdup(str);
+	ptr->str = s;
+	ptr->next = *head;
+	return (&ptr);
+}
---
+++ 0x12-singly_linked_lists/test/README.md
@@ -0,0 +1,1 @@
+this repository contains functions manipulating singly linked lists.
