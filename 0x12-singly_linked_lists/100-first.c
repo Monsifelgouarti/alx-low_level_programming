bef15377cb9a035151498fb7795f7954a77b3bea
Monsifelgouarti <monsif.cun@gmail.com>
2023-05-01 16:28:42 -0700

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * premain - code executed before main
+ * beforemain - code executed before main
  *
  * Return: Always 0.
  */
 void __attribute__ ((constructor)) beforemain()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
