cbec932a1660c8f05d43e93a3803f0ddc5876fe5
Monsifelgouarti <monsif.cun@gmail.com>
2023-05-01 16:29:35 -0700

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void __attribute__ ((constructor)) beforemain()
 {
-	printf("You're beat! and yet, you must allow,
+	printf("You're beat! and yet, you must allow,\
 			\nI bore my house upon my back!\n");
 }
