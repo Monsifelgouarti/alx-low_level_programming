e8211c2afff7babe344306e8e8c4cf3140c40fb1
Monsifelgouarti <monsif.cun@gmail.com>
2023-05-01 16:27:14 -0700

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,17 +4,7 @@
  *
  * Return: Always 0.
  */
-void __attribute__ ((constructor)) premain()
+void __attribute__ ((constructor)) beforemain()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
 }
