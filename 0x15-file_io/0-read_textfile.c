a1b5044c0f402e5f85628b7c0fd7758930a3a08a
Monsifelgouarti <monsif.cun@gmail.com>
2023-05-08 16:49:25 -0700

    read file and print to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,23 +1,30 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: the name of file to read.
  * @letters:  the number of letters it should read and print.
  * Return: the actual number of letters it could read and print.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+size_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *pf;
-	int lp = 0;
-	char c;
-	
-	if ((fp = fopen(filename, "r")) == NULL || filename == NULL)
-		return (0);
+	FILE *fp;
+	size_t lp = 0;
+	char *c;
 
-	while ((c = getc(fp)) != feof)
+	if (filename == NULL)
+		return (0);
+	fp = fopen(filename, "r");
+	if (!fp)
+		return (0);
+	c = malloc(sizeof(char));
+	while (lp < letters)
 	{
+		*c = getc(fp);
 		write(STDOUT_FILENO, c, 1);
 		lp++;
 	}
+	fclose(fp);
 	return (lp);
 }
