215eab5b7063e015676367e5cab33de7c060914f
Monsifelgouarti <monsif.cun@gmail.com>
2023-05-09 16:20:55 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,21 +10,20 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	FILE *fp;
-	size_t lp = 0;
 	int i = 0;
 
 	if (filename == NULL)
 		return (-1);
+	if (access(filename, F_OK) != 0)
+		return (-1);
 	fp = fopen(filename, "r");
 	if (!fp)
-		return (0);
-	i = access(filename, W_OK);
-	if (i != 0)
+		return (-1);
+	if (access(filename, W_OK) != 0)
 	{
 		fclose(fp);
 		return (-1);
 	}
-	i = 0;
 	if (text_content != NULL)
 	while (text_content[i] != '\0')
 	{
@@ -32,5 +31,5 @@ int append_text_to_file(const char *filename, char *text_content)
 		i++;
 	}
 	fclose(fp);
-	return (lp);
+	return (1);
 }
