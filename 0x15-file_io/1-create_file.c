333c8c15ce44b2ea1610229fdb27f109cfad2d33
Monsifelgouarti <monsif.cun@gmail.com>
2023-05-09 16:09:23 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,24 @@
 int create_file(const char *filename, char *text_content)
 {
 	FILE *fp;
-	int i = 0;
+	int i = 0, stat, f;
 
 	if (filename == NULL)
 		return (-1);
-	fp = fopen(filename, "wr");
+	stat = access(filename, F_OK);
+	if (stat != 0)
+	{
+		f = open(filename, O_WRONLY | O_CREAT 
+				| O_TRUNC, S_IRUSR | S_IWUSR);
+		if (f == -1)
+			return (-1);
+	}
+	stat = access(filename, R_OK);
+	if (stat != 0)
+		return (-1);
+	fp = fopen(filename, "w");
 	if (!fp)
-		return (0);
+		return (-1);
 	if (text_content != NULL)
 	while (text_content[i] != '\0')
 	{
