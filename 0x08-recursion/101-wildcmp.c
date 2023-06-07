7872ceab965fcbcdf84f6086a57cb9922f47bd00
Monsifelgouarti <monsif.cun@gmail.com>
2023-06-07 16:31:10 -0700

    wild cmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+/**
+ * wildcmp - compare two strings
+ * @s1: a string
+ * @s2: a string
+ * Return: reurn 1 if the two strings is identical otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' || *s2 == '\0')
+		return (1);
+	if (*s2 == '*')
+	{
+		s2++;
+		wildcmp(s1, s2);
+	}
+	if (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+		wildcmp(s1, s2);
+	}
+	return (0);
+}
