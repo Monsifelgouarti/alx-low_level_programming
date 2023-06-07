0bc357862eb4507a353d498acc4872d08c006221
Monsifelgouarti <monsif.cun@gmail.com>
2023-06-07 16:46:41 -0700

    wild cmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,12 +18,9 @@ int wildcmp(char *s1, char *s2)
 		s2++;
 		wildcmp(s1, s2);
 	}
-	if (*s1 == *s2)
-	{
-		s1++;
-		s2++;
-		wildcmp(s1, s2);
-	}
-	else
+	if (*s1 != *s2)
 		return (0);
+	s1++;
+	s2++;
+	wildcmp(s1, s2);
 }
