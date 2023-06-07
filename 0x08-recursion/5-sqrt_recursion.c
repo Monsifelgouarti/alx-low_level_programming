701ac60f583638a4a69f5d639dc4391393358fdc
Monsifelgouarti <monsif.cun@gmail.com>
2023-06-07 14:47:39 -0700

    sqrt root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * _sqrt_recursion -  the natural sqrt root of a number.
+ * @n: a number
+ * Return: sqrt root of @n or -1.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	else
+		return (sqrt_root(n, 1));
+}
+/**
+ * sqrt_root - test if @y is a sqrt root to @n.
+ * @n: a number
+ * @y: a number
+ * Return: sqrt root of @n or -1.
+ */
+int sqrt_root(int n, int y)
+{
+	if (y >= n)
+		return (-1);
+	if (n == (y * y))
+		return (y);
+	else
+		return (sqrt_root(n, y + 1));
+}
