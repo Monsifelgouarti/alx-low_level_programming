56335b11bffa082c78b962b23557b9e7164a345d
Monsifelgouarti <monsif.cun@gmail.com>
2023-06-08 15:35:47 -0700

    wild cmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,4 @@
+int _strlen_recursion(char *s);
 /**
  * wildcmp - compare two strings
  * @s1: a string
@@ -12,12 +13,26 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(++s1, ++s2));
 	else if (*s2 == '*')
 	{
-		if (*(s2 + 1) == '*' || *s1 == '\0' || *s1 == *(s2 + 1))
-		s2++;
+		if (*(s2 + 1) == '*' || *s1 == '\0')
+			s2++;
 		else if (*s1 != *(s2 + 1))
 			s1++;
+		else if (*s1 == *(s2 + 1))
+			s2++;
 		return (wildcmp(s1, s2));
 	}
 	else
 		return (0);
+}
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: a string
+ * Return: the length of @s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(++s));
 }
