70eed99298209c2e4ded623063a09f02d93cd19d
Monsifelgouarti <monsif.cun@gmail.com>
2023-06-09 10:58:23 -0700

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argv: array of arguments
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
 
 	if (argc >= 3)
 	{
-		i = a toi(argv([1]) * atoi(argv[2]);
+		i = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", i);
 		return (0);
 	}
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -29,5 +29,7 @@ int main(int argc, char *argv[])
 			}
 			printf("%d\n", sum);
 		}
-		return (0);
+	}
+
+	return (0);
 }
