d1fc2cd6dcd2801ba8d66d867190c733a5f31b7b
Monsifelgouarti <monsif.cun@gmail.com>
2023-07-06 11:56:03 -0700

    clear bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: a number
+ * @index: index of bit to return
+ * Return: value of the bit at @index or -1;
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index < 31)
+	{
+		n >>= index;
+		return (n & 1 ? 1 : 0);
+	}
+	return (-1);
+}
